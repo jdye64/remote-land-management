473beabdb134c153051c27c0fb35b09f4d597702
Jeremy Dyer <jdye64@gmail.com>
2024-06-01 10:50:14 -0400

    gitignore update

--- src/main.py
+++ src/main.py
@@ -5,19 +5,25 @@ import time
 GPIO.setmode(GPIO.BOARD)
 
 # Set GPIO pin 8 as an output
-GPIO.setup(8, GPIO.OUT)
+GPIO.setup(7, GPIO.OUT)
+GPIO.output(7, GPIO.HIGH)
 
-try:
-    # Turn on GPIO pin 8
-    GPIO.output(8, GPIO.HIGH)
-    print("GPIO pin 8 is ON")
+from fastapi import FastAPI
+
+# Create an instance of the FastAPI class
+app = FastAPI()
+
+# Define a GET endpoint
+@app.get("/on")
+async def read_root():
+    # Turn on GPIO pin 4
+    GPIO.output(7, GPIO.LOW)
+    print("GPIO pin 7 is ON")
     time.sleep(4)  # Wait for 1 second
 
-    # Turn off GPIO pin 8
-    GPIO.output(8, GPIO.LOW)
-    print("GPIO pin 8 is OFF")
+    # Turn off GPIO pin 4
+    GPIO.output(7, GPIO.HIGH)
+    print("GPIO pin 7 is OFF")
     time.sleep(1)  # Wait for 1 second
 
-finally:
-    # Clean up
-    GPIO.cleanup()
+    return {"Hello": "World"}
